403a2649d2accabc2a7b93ce97396d6814e1e3f6
LianNidam <liannidam04@gmail.com>
2022-03-31 23:32:34 +0300

    Weather forecast for each city

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
-  const query= "Paris";
+app.post("/", function(req,res){
+
+  const query= req.body.cityName;
   const appIdKey= "f2895ea75bdf6aa1506595f37f88814f";
-  const unit= "metric";
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=" +query+ "&appid=" +appIdKey+ " &units=" +unit;
+  //const unit= "metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" +query+ "&appid=" +appIdKey+  "&units=metric";
+
+//const url = "https://api.openweathermap.org/data/2.5/weather?q=" +  cityname +  "&appid=" +keyid + "&units=metric";
+
   https.get(url, function(response) {
     console.log(response.statusCode);
 
     response.on("data", function(data) {
       const weatherData = JSON.parse(data);
-      const temp = weatherData.main.temp
-      const weatherDescription = weatherData.weather[0].description
-      const icon = weatherData.weather[0].icon
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
       const imageURL= "http://openweathermap.org/img/wn/"+ icon +"@2x.png";
 
       res.write("<p>The weather is currenly "+ weatherDescription+ "</p>");
@@ -26,9 +35,7 @@ app.get("/", function(req, res) {
       res.send();
     });
   });
-
 });
-
 
 
 
