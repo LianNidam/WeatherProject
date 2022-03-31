9ff45cfcd58dee380b64ec8dc9dd51d9f173264c
LianNidam <liannidam04@gmail.com>
2022-03-31 23:37:36 +0300

    Weather forecast for each city

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.post("/", function(req,res){
   const query= req.body.cityName;
   const appIdKey= "f2895ea75bdf6aa1506595f37f88814f";
   //const unit= "metric";
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=" +query+ "&appid=" +appIdKey+  "&units=metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" +query+ "&appid=" +appIdKey+ "&units=metric";
 
 //const url = "https://api.openweathermap.org/data/2.5/weather?q=" +  cityname +  "&appid=" +keyid + "&units=metric";
 
@@ -25,12 +25,12 @@ app.post("/", function(req,res){
     response.on("data", function(data) {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
-      const weatherDescription = weatherData.weather[0].description;
-      const icon = weatherData.weather[0].icon;
+      const weatherDescription = weatherData.weather[0].description
+      const icon = weatherData.weather[0].icon
       const imageURL= "http://openweathermap.org/img/wn/"+ icon +"@2x.png";
 
       res.write("<p>The weather is currenly "+ weatherDescription+ "</p>");
-      res.write("<h1>The temperature in Tel Aviv is " + temp + " degrees Celcius.</h1>");
+      res.write("<h1>The temperature in " +query+" is " + temp + " degrees Celcius.</h1>");
       res.write("<img src=" + imageURL + ">");
       res.send();
     });
